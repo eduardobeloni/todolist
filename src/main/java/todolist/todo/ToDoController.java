ace2a47519ee710ae28842010e6e1791dfc94001
Eduardo Fiss Beloni <eduardobeloni@yahoo.com.br>
2021-05-19 10:55:29 -0300

    ToDoController: using RequestParam annotation

--- src/main/java/todolist/todo/ToDoController.java
+++ src/main/java/todolist/todo/ToDoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,9 +26,21 @@ public class ToDoController {
 		return toDoService.getToDo(id);
 	}
 	
+	@RequestMapping(method=RequestMethod.GET, value="/todos/param")
+	public ToDo getToDoParam(@RequestParam Integer id) {
+		return this.getToDo(id);
+	}
+
 	@RequestMapping(method=RequestMethod.POST, value="/todos")
 	public void createToDo(@RequestBody ToDo todo) {
 		toDoService.createToDo(todo);
+	}
+
+	@RequestMapping(method=RequestMethod.POST, value="/todos/param")
+	// No need for an annotation before todo.
+	// This is called a form object that matches the name of ToDo fields in the parameters.
+	public void createToDoParam(ToDo todo) {
+		this.createToDo(todo);
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT, value="/todos/{id}")
@@ -35,8 +48,18 @@ public class ToDoController {
 		toDoService.updateToDo(id, todo);
 	}
 	
+	@RequestMapping(method=RequestMethod.PUT, value="/todos/param")
+	public void updateToDoParam(ToDo todo) {
+		this.updateToDo(todo.getId(), todo);
+	}
+
 	@RequestMapping(method=RequestMethod.DELETE, value="/todos/{id}")
 	public void deleteToDo(@PathVariable Integer id) {
 		toDoService.deleteToDo(id);
+	}
+
+	@RequestMapping(method=RequestMethod.DELETE, value="/todos/param")
+	public void deleteToDoParam(@RequestParam Integer id) {
+		this.deleteToDo(id);
 	}
 }
